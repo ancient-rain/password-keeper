4716bc6c86a09b95eceb2e6fcf34023d8415c660
askalerion <bednartd@rose-hulman.edu>
2017-06-28 18:30:19 -0400

    boilerplate setup to add three common packages

--- PasswordKeeper/src/environments/environment.prod.ts
+++ PasswordKeeper/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAxbHE3k89cTYETJjsfZ1cxr0i4ge6HPSg",
+    authDomain: "bednartd-password-keeper.firebaseapp.com",
+    databaseURL: "https://bednartd-password-keeper.firebaseio.com",
+    projectId: "bednartd-password-keeper",
+    storageBucket: "bednartd-password-keeper.appspot.com",
+    messagingSenderId: "616704615739"
+  },
 };
--- PasswordKeeper/src/environments/environment.ts
+++ PasswordKeeper/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAxbHE3k89cTYETJjsfZ1cxr0i4ge6HPSg",
+    authDomain: "bednartd-password-keeper.firebaseapp.com",
+    databaseURL: "https://bednartd-password-keeper.firebaseio.com",
+    projectId: "bednartd-password-keeper",
+    storageBucket: "bednartd-password-keeper.appspot.com",
+    messagingSenderId: "616704615739"
+  },
 };
